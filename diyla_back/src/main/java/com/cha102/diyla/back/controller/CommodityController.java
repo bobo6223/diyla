883ec2bf8cd9e175e98b5914e608c67b8d1e8f3d
bobo6223 <91510156+bobo6223@users.noreply.github.com>
2023-07-26 19:15:04 +0800

    shop開發

--- diyla_back/src/main/java/com/cha102/diyla/back/controller/CommodityController.java
+++ diyla_back/src/main/java/com/cha102/diyla/back/controller/CommodityController.java
@@ -9,13 +9,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/shop/CommodityController")
 public class CommodityController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-//        PrintWriter writer = resp.getWriter();
-//        writer.println("hi");
+        PrintWriter writer = res.getWriter();
+        writer.println("hi");
     }
 
     @Override
