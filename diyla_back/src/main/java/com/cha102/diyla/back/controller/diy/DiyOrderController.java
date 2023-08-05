2fad912c44beaf1f05ccbc5d9f09c452c8221cbe
LaeverLee <encorec42923@gmail.com>
2023-08-05 17:30:52 +0800

    FIX

---
+++ diyla_back/src/main/java/com/cha102/diyla/back/controller/diy/DiyOrderController.java
@@ -0,0 +1,27 @@
+package com.cha102.diyla.back.controller.diy;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/latestnews/diyODServlet")
+public class DiyOrderController extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		doPost(req, res);
+	}
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+
+		req.setCharacterEncoding("UTF-8");
+		String action = req.getParameter("action");
+		String newsNo = req.getParameter("diyorderno");
+	}
+
+}
